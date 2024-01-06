682dd6cad63e9dcc2e991fd4ca7c1e7b9009b58d
CodeJedi <34801810+codejedi-ai@users.noreply.github.com>
2024-01-06 15:31:38 -0500

    Put the GMM segmentation in a python object

--- MyGraphCuts.py
+++ MyGraphCuts.py
@@ -6,7 +6,7 @@ from skimage import img_as_ubyte
 from sklearn import mixture
 from skimage.color import rgb2gray
 import os
-import GMM
+'''
 def wpq(lambda_, p, q, sigma):
   return lambda_ * np.exp(- ((p - q) ** 2) / sigma ** 2)
 
@@ -16,11 +16,7 @@ def wpq_vector(lambda_, p, q, sigma):
 def wpq_vector2(lambda_, p, q, sigma):
     d = p-q
     return lambda_ * np.exp(-((d[...,0]**2)+(d[...,1]**2)+(d[...,2]**2)) / sigma ** 2)
-
-img1 = plt.imread('images/Meki.png')
-wpq_vector(1, img1, np.roll(img1, -1, axis = 1), 5).shape
-
-
+'''
 class MyGraphCuts:
     bgr_value = 0
     obj_value = 1
@@ -34,9 +30,76 @@ class MyGraphCuts:
         self.sigma = sigma
         self.img = img_as_ubyte(img)
         self.lambda_ = lambda_
-
+        self.gmm1 = mixture.GaussianMixture(n_components=7, covariance_type='full')
+        self.gmm0 = mixture.GaussianMixture(n_components=7, covariance_type='full')
         self.g, self.nodeids = self.calculate_graph()
 
+    import numpy as np
+
+    def _extract_colors(self, foreground_slices_list, background_slices_list):
+        im = self.img
+        # Extract the RGB channels from the image
+        R = im[:, :, 0].flatten()
+        G = im[:, :, 1].flatten()
+        B = im[:, :, 2].flatten()
+
+        # Initialize empty lists for foreground and background colors
+        R_foreground = []
+        G_foreground = []
+        B_foreground = []
+        R_background = []
+        G_background = []
+        B_background = []
+
+        # Extract the foreground colors using the given slices
+        for foreground_slices in foreground_slices_list:
+            R_foreground.extend(
+                im[foreground_slices[0]:foreground_slices[1], foreground_slices[2]:foreground_slices[3], 0].flatten())
+            G_foreground.extend(
+                im[foreground_slices[0]:foreground_slices[1], foreground_slices[2]:foreground_slices[3], 1].flatten())
+            B_foreground.extend(
+                im[foreground_slices[0]:foreground_slices[1], foreground_slices[2]:foreground_slices[3], 2].flatten())
+
+        # Extract the background colors using the given slices
+        for background_slices in background_slices_list:
+            R_background.extend(
+                im[background_slices[0]:background_slices[1], background_slices[2]:background_slices[3], 0].flatten())
+            G_background.extend(
+                im[background_slices[0]:background_slices[1], background_slices[2]:background_slices[3], 1].flatten())
+            B_background.extend(
+                im[background_slices[0]:background_slices[1], background_slices[2]:background_slices[3], 2].flatten())
+
+        # Return the extracted colors
+        # return (R_foreground, G_foreground, B_foreground), (R_background, G_background, B_background)
+
+        # foreground_colors, background_colors = extract_colors(im, foreground_slices_list, background_slices_list)
+
+        R1, G1, B1 = (R_foreground, G_foreground, B_foreground)
+        R0, G0, B0 = (R_background, G_background, B_background)
+        # set of colors for all image pixels
+        R = im[:, :, 0].flatten()
+        G = im[:, :, 1].flatten()
+        B = im[:, :, 2].flatten()
+        data1 = np.vstack([R1, G1, B1])
+        data0 = np.vstack([R0, G0, B0])
+        data = np.vstack([R, G, B])
+        return data0, data1, data
+
+    def _GMM(self, data0, data1, data):
+        im = self.img
+        # estimate color distributions using GMM - should be fast (1-2 seconds for both models)
+        from sklearn import mixture
+
+        gmm1 = self.gmm1
+        gmm0 = self.gmm0
+        gmm1.fit(data1.T)
+        gmm0.fit(data0.T)
+        like1 = -np.array(gmm1.score_samples(data.T))
+        like0 = -np.array(gmm0.score_samples(data.T))
+        like1_im = like1.reshape((im.shape[0], im.shape[1]))
+        like0_im = like0.reshape((im.shape[0], im.shape[1]))
+        # always forground then background 0 then 1
+        return like0_im, like1_im
     def calculate_graph(self):
         img = self.img
         lambda_ = self.lambda_
@@ -82,8 +145,9 @@ class MyGraphCuts:
         # |         |         |
         # +---------+---------+
         foreground_slices_list, background_slices_list = seed_mask
-        data0, data1, data = GMM.extract_colors(self.img, foreground_slices_list, background_slices_list)
-        like0_im, like1_im = GMM.GMM(self.img, data0, data1, data)
+        data0, data1, data = self._extract_colors(foreground_slices_list, background_slices_list)
+        like0_im, like1_im = self._GMM(data0, data1, data)
+        g, nodeids = self.g, self.nodeids
         # print(n_right)
         # print(n_below)
         g.add_grid_tedges(nodeids, like0_im, like1_im)
@@ -103,12 +167,13 @@ class MyGraphCuts:
         # |         |         |
         # +---------+---------+
         data0, data1 = seed_mask
-
+        im = self.img
+        g, nodeids = self.g, self.nodeids
         R = im[:, :, 0].flatten()
         G = im[:, :, 1].flatten()
         B = im[:, :, 2].flatten()
         data = np.vstack([R, G, B])
-        like0_im, like1_im = GMM.GMM(self.img, data0, data1, data)
+        like0_im, like1_im = self._GMM(data0, data1, data)
         # print(n_right)
         # print(n_below)
         g.add_grid_tedges(nodeids, like0_im, like1_im)
@@ -122,53 +187,6 @@ class MyGraphCuts:
 
 
 
-img = 'Meki2.png'
-curfolder = os.path.dirname(os.path.abspath(__file__))
-im = plt.imread(os.path.join(curfolder, 'images', img))
-# display the graph
-# set the center of the image to be the source
-# set the edges to be the weights
-
-
-graphcut = MyGraphCuts(im, 0.01, 0.005)
-
-foreground_slices_list = [(150, 250, 250, 300), (300, 500, 200, 300), (0, 200, 100, 300)]
-background_slices_list = [(50, 350, 20, 50), (0, 300, 400, 500), (300, 500, 0, 80)]
-
-g, nodeids = graphcut.g, graphcut.nodeids
-
-labels = graphcut.compute_labels_mask((foreground_slices_list, background_slices_list))
-plt.imsave(os.path.join(curfolder, "matplot", 'labels.png'), labels)
 
 
 
-
-
-'''z
-
-img1 = plt.imread('images/Meki.png')
-app = MyGraphCuts(img1, 50, 10)
-app.run()
-
-
-
-img1 = plt.imread('images/bunny.bmp')
-app = MyGraphCutsCoulorBasedLikelihood(img1, 50, 20, [1,2,3], [1,2,3])
-app.run()
-
-
-sigma = [0.01, 0.1, 1, 10, 100]
-img1 = plt.imread('images/bunny.bmp')
-print('Sigma used:', sigma[1])
-app = MyGraphCuts(img1, sigma[0], 70, 40)
-app.run()
-
-img2 = plt.imread('images/lama.jpg')
-app = MyGraphCuts(img2, sigma = 1, lambda_ = 0)
-app.run()
-
-img2 = plt.imread('images/lama.jpg')
-app = MyGraphCuts(img2, sigma = 1, 3)
-app.run()
-
-'''
---
+++ main.py
@@ -0,0 +1,27 @@
+import MyGraphCuts
+import matplotlib.pyplot as plt
+import os
+img = 'Meki2.png'
+curfolder = os.path.dirname(os.path.abspath(__file__))
+im_folder = os.path.join(curfolder, 'images')
+# iterate through all the images in the folder
+# get the list of images
+img_list = os.listdir(im_folder)
+print(img_list)
+for i in range(len(img_list)):
+    im = plt.imread(os.path.join(im_folder, img_list[i]))
+    # display the graph
+    # set the center of the image to be the source
+    # set the edges to be the weights
+
+    graphcut = MyGraphCuts.MyGraphCuts(im, 0.01, 0.005)
+
+    foreground_slices_list = [(150, 250, 250, 300), (300, 500, 200, 300), (0, 200, 100, 300)]
+    background_slices_list = [(50, 350, 20, 50), (0, 300, 400, 500), (300, 500, 0, 80)]
+
+    g, nodeids = graphcut.g, graphcut.nodeids
+
+    labels = graphcut.compute_labels_mask((foreground_slices_list, background_slices_list))
+    # get the name of the img
+    img_name = img_list[i].split('.')[0]
+    plt.imsave(os.path.join(curfolder, "matplot", f'{img_name}_labels.png'), labels)
